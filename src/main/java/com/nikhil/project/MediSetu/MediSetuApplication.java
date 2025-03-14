fad2e24b697277ad0aae6dfd2e482647fc5c06d5
KnightNikhil <nikhil.the.developer@gmail.com>
2025-03-14 14:28:41 +0530

    Initial commit

---
+++ src/main/java/com/nikhil/project/MediSetu/MediSetuApplication.java
@@ -0,0 +1,13 @@
+package com.nikhil.project.MediSetu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediSetuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediSetuApplication.class, args);
+	}
+
+}
